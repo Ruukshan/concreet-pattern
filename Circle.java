17093c81b95690e0d6fc662e416098790e350c16
Ruukshan <rukshaninduwara123@gmail.com>
2024-08-22 22:16:56 +0530

    Task 3

---
+++ Circle.java
@@ -0,0 +1,10 @@
+public class Circle implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Circle:");
+        System.out.println(" ** ");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println(" ** ");
+    }
+}
---
+++ Square.java
@@ -0,0 +1,10 @@
+public class Square implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Drawing a Square:");
+        System.out.println("****");
+        System.out.println("*  *");
+        System.out.println("*  *");
+        System.out.println("****");
+    }
+}
